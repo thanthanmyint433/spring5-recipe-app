4d6e204e76293ecd9b1724ea4716079c72fe81d6
nanmokham <nanmokham@isgm2.com>
2020-05-20 11:43:11 +0630

    commit deleting ingredient

--- src/test/java/guru/springframework/spring5recipeapp/controllers/IngredientControllerTest.java
+++ src/test/java/guru/springframework/spring5recipeapp/controllers/IngredientControllerTest.java
@@ -133,4 +133,11 @@ public class IngredientControllerTest {
         verify(recipeService, times(1)).findCommandById(anyLong());
 
     }
+
+    @Test
+    public void testDeleteIngredient() throws Exception{
+        mockMvc.perform(get("/recipe/2/ingredient/3/delete"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/recipe/2/ingredients"));
+    }
 }
